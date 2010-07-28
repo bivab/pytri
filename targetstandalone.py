e5e4b85775719e39fdca7f293e2a2de11a9838f7
David Schneider <david.schneider@picle.org>
2010-07-28 16:44:22 +0200

    get net and state from the first line passed to the commandline

--- targetstandalone.py
+++ targetstandalone.py
@@ -1,24 +1,37 @@
 import sys
+import os
 # __________  Entry point  __________
 from expression import NumericExpression, VariableExpression
 from petri_net import PetriNet
 from propositions import LessProposition, EUProposition, EqualsProposition
 from transition import Transition
 from state import State
+from parse import parse_net
 
 def entry_point(argv):
-    t = Transition([0], [1])
-    p = PetriNet([t])
-    i = 1000000
-    state = State([i,0], p)
+    file = argv[1]
+    p, state = parse_net(read_file(file))
     prop = EUProposition(LessProposition(NumericExpression(0),
     VariableExpression(0)), EqualsProposition(VariableExpression(1),
-    NumericExpression(i)))
+    NumericExpression(1000000)))
     if state.evaluate(prop):
         print "True"
     else:
         print "False"
     return 0
+
+def read_file(filename):
+    fd = os.open(filename, os.O_RDONLY, 0777)
+    content = []
+    while 1:
+        s = os.read(fd, 4096)
+        if not s:
+            break
+        content.append(s)
+    file_content = "".join(content)
+    os.close(fd)
+    return file_content
+
 # _____ Define and setup target ___
 
 
